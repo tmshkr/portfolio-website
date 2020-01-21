2f963b55fce6293280e1d78b44063c21e6e630a1
Tim Shaker <tim@tmshkr.com>
2020-01-21 13:04:52 -0800

    buttons

--- resize-images.js
+++ resize-images.js
@@ -2,14 +2,14 @@ const sharp = require("sharp");
 const path = require("path");
 const fs = require("fs");
 
-fs.readdirSync(path.join(__dirname, "images/source")).forEach(file => {
-  if (/.jpg$/.test(file)) {
-    sharp(path.join(__dirname, "images/source", file))
-      .resize({ width: 1800, height: 1200 })
+fs.readdirSync(path.join(__dirname, "images/projects")).forEach(file => {
+  if (/.png$/.test(file)) {
+    sharp(path.join(__dirname, "images/projects", file))
+      .resize({ width: 200, height: 200 })
       .toFile(path.join(__dirname, "images/resized", file))
       .then(info => {
         const oldPath = path.join(__dirname, "images/resized", file);
-        const newPath = oldPath.replace(".jpg", `-${info.width}w.jpg`);
+        const newPath = oldPath.replace(".png", `-${info.width}w.png`);
         fs.renameSync(oldPath, newPath);
         console.log(file, "resized");
       })
